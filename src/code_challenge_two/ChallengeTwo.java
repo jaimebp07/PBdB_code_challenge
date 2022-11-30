125058896e3436319bcd51bb9a2a93d19ca29f85
andres <andres@DESKTOP-1KMLSUD.mshome.net>
2022-11-29 22:25:42 -0500

    Se crean los contructores y los metodos main para cada uno de los ejecicios

--- src/code_challenge_two/ChallengeTwo.java
+++ src/code_challenge_two/ChallengeTwo.java
@@ -2,4 +2,11 @@ package code_challenge_two;
 
 public class ChallengeTwo {
 
+	public ChallengeTwo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		new ChallengeTwo();
+	}
 }
